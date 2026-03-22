f6162775f78b4cb54578fff1ca4b9dfda1972d2f
ntranhtuan <ntranhtuanl@gmail.com>
2026-03-22 11:40:06 +0700

    update test.py - fix dominate module and GPU configuration

--- test.sh
+++ test.sh
@@ -1,1 +1,3 @@
-python test.py --dataroot datasets/reflection --name IBCLN --model IBCLN --dataset_mode resize_natural_3  --preprocess "" --no_flip --epoch final --gpu_ids 0``
+#!/bin/bash
+source .venv/bin/activate
+python test.py --dataroot datasets/reflection --name IBCLN --model IBCLN --dataset_mode resize_natural_3  --preprocess "" --no_flip --epoch final --gpu_ids -1
